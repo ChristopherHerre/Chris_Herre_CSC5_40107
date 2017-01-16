492476c04a07189743beea60052c39e5d3ffee5a
ChristopherHerre <cherre@student.rcc.edu>
2017-01-16 05:22:13 -0800

    Initial commit

--- Hmwk/Assignment_3/Gaddis_8thEd_Chap4_Prob2_RomanNumerals/main.cpp
+++ Hmwk/Assignment_3/Gaddis_8thEd_Chap4_Prob2_RomanNumerals/main.cpp
@@ -4,6 +4,14 @@
  * Author: Chris Herre
  *
  * Created on January 14, 2017, 9:23 PM
+ * 
+ * 2. Roman Numeral Converter
+ * Write a program that asks the user to enter a number within the range of 1
+ * through 10. Use a switch statement to display the Roman numeral version of
+ * that number.
+ * 
+ * Input Validation: Do not accept a number less than 1 or greater than 10.
+ * 
  */
 
 #include <cstdlib>
@@ -15,9 +23,11 @@ int main(int argc, char** argv) {
     short input;
     string output;
     
+    // prompt user for input
     cout << "Enter a number between 1 and 10: ";
     cin >> input;
     
+    // handle all the possible values for input
     switch (input)
     {
         case 1:
@@ -55,6 +65,7 @@ int main(int argc, char** argv) {
             return 0;
     }
     
+    // output the resulting Roman numeral for the given input
     cout << output << ". This is the Roman numeral of " << input << "." << endl;
     return 0;
 }
