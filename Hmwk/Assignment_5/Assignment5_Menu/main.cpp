32b3db127ea717c28647f2d99021076bbd7287ae
root <root@localhost.localdomain>
2017-02-01 12:47:54 -0800

    Initial commit

--- Hmwk/Assignment_5/Assignment5_Menu/main.cpp
+++ Hmwk/Assignment_5/Assignment5_Menu/main.cpp
@@ -4,6 +4,9 @@
  * Author: Chris Herre
  *
  * Created on February 1, 2017, 1:27 PM
+ * 
+ * Notice: Use C++ 11 compiler
+ * 
  */
 
 #include <cstdlib>
@@ -27,10 +30,26 @@ void safestDrivingArea();
 int getNumAccidents(string);
 void findLowest(short []);
 
-const string REGIONS[] = { "north", "south", "east", "west", "central" };
+void fallingDist();
+float fallingDistance(short);
 
-const string DIVISIONS[] = 
-    { "Northeast", "Southeast", "Northwest", "Southwest" };
+void displayKEnergy();
+float kineticEnergy(float, float);
+
+void displayCelsius();
+float celsius(float);
+
+void doCoinToss();
+void coinToss();
+
+void doPresentValue();
+float presentValue(float, float, short);
+
+void doFutureValue();
+float futureValue(float, float, short);
+
+void populationSize();
+float getPopSize(int, float, float);
 
 int main(int argc, char** argv) {
     short choice;
@@ -50,6 +69,27 @@ int main(int argc, char** argv) {
                 break;
             case 3:
                 safestDrivingArea();
+                break;
+            case 4:
+                fallingDist();
+                break;
+            case 5:
+                displayKEnergy();
+                break;
+            case 6:
+                displayCelsius();
+                break;
+            case 7:
+                doCoinToss();
+                break;
+            case 8:
+                doPresentValue();
+                break;
+            case 9:
+                doFutureValue();
+                break;
+            case 10:
+                populationSize();
                 break;
         }
     } while (choice > 0 && choice <= 10);
@@ -108,6 +148,9 @@ float calculateRetail(float cost, float markup)
 
 void winningDivision()
 {
+    const string DIVISIONS[] = 
+    { "Northeast", "Southeast", "Northwest", "Southwest" };
+    
     // create an array to store the sales of each division
     float sales[4];
     
@@ -152,6 +195,9 @@ float getSales(string div)
 // prints the highest value given an array reference
 void findHeighest(float totals[])
 {
+    const string DIVISIONS[] = 
+    { "Northeast", "Southeast", "Northwest", "Southwest" };
+    
     float highest = 0;
     unsigned char highIndex = 0;
     
@@ -174,6 +220,7 @@ void findHeighest(float totals[])
 
 void safestDrivingArea()
 {
+    const string REGIONS[] = { "north", "south", "east", "west", "central" };
     short numAccidents[5];
 
     // populate numAccidents array with the return value of getNumAccidents
@@ -206,6 +253,7 @@ int getNumAccidents(string region)
 
 void findLowest(short accidents[])
 {
+    const string REGIONS[] = { "north", "south", "east", "west", "central" };
     // set lowest to a value larger than any inputs to the accidents array
     short lowest = 30000;
     unsigned char lowestIndex = 0;
@@ -224,4 +272,191 @@ void findLowest(short accidents[])
     // output which region had the least accidents
     cout << "The " << REGIONS[lowestIndex] << " region had the least amount of"
             " accidents at " << lowest << " accidents last year." << endl;
+}
+
+void fallingDist()
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << "Distance: " << fallingDistance(i) << endl;
+    }
+    return;   
+}
+
+float fallingDistance(short time)
+{
+    return 0.5 * 9.8 * (time * time);
+}
+
+void displayKEnergy()
+{
+    float mass, vel;
+    
+    cout << "Enter the objects mass in kg: ";
+    cin >> mass;
+    
+    cout << "Enter the objects velocity in m/s: ";
+    cin >> vel;
+    
+    cout << "Kinetic Energy = " << kineticEnergy(mass, vel) << " Joules"
+            << endl;
+        
+}
+
+float kineticEnergy(float mass, float vel)
+{
+    return 0.5 * mass * (vel * vel);
+}
+
+void displayCelsius()
+{
+    for (int i = 0; i <= 20; i++)
+    {
+        cout << i << " Fahrenheit\t=\t" << celsius(i) << " Celsius" << endl;
+    }
+}
+
+float celsius(float fahr)
+{
+    return 5/9.0 * (fahr - 32);
+}
+
+void doCoinToss()
+{
+    srand(static_cast<unsigned int>(time(0)));
+    
+    short flips;
+    
+    cout << "How many coin tosses? ";
+    cin >> flips;
+    
+    cout << flips << endl;
+    
+    for (short i = 0; i < flips; i++)
+    {
+        cout << i + 1 << "\t";
+        coinToss();
+    }
+
+}
+
+void coinToss()
+{
+    short toss = (rand() % 2) + 1;
+    
+    cout << (toss == 1 ? "Heads" : "Tales") << endl;    
+}
+
+void doPresentValue()
+{
+    float p, f, r, years;
+    
+    cout << "Enter the desired future value: ";
+    cin >> f;
+    
+    cout << "Enter the annual interest rate: ";
+    cin >> r;
+    
+    cout << "Enter the number of years: ";
+    cin >> years;
+    
+    float present = presentValue(f, r, years);
+    cout << "Present Value: " << present << endl;    
+}
+
+float presentValue(float future, float interestRate, short years)
+{
+    return future / (pow(1.0 + interestRate, years));
+}
+
+void doFutureValue()
+{
+    float f, p, i;
+    short t;
+    
+    cout << "Enter the accounts present value: ";
+    cin >> p;
+    
+    cout << "Enter the monthly interest rate: ";
+    cin >> i;
+    
+    cout << "Enter the number of months: ";
+    cin >> t;
+    
+    f = futureValue(p, i, t);
+    cout << "Future value: " << f << endl;    
+}
+
+float futureValue(float p, float i, short t)
+{
+    return p * pow((1 + i), t);
+}
+
+void populationSize()
+{
+    int p = 0;
+    float birth = 0.0f, death = 0.0f;
+    short years = 0;
+    
+    // prompt user to input number of years
+    cout << "Enter the number of years: ";
+    cin >> years;
+
+    // if years less than 1 then exit
+    if (years < 1)
+    {
+        cout << "Years must be at least 1!" << endl;
+        exit(0);        
+    } 
+    
+    for (int i = 0; i < years; i++)
+    {
+    
+        if (p == 0)
+        {
+            // prompt user for starting size
+            cout << "Enter the starting population size: ";
+            cin >> p;
+
+            // starting size must be greater than 2 or else exit
+            if (p < 2)
+            {
+                cout << "Starting population size must be greater than 2!"
+                        << endl;
+                exit(0);
+            }
+        }
+
+        // prompt user for birth rate
+        cout << "Enter the annual birth rate: ";
+        cin >> birth;
+
+        // validate birth rate
+        if (birth < 1.0)
+        {
+            cout << "Birth rate must be greater than 1.0!" << endl;
+            exit(0);        
+        }
+
+        // prompt user for death rate
+        cout << "Enter the annual death rate: ";
+        cin >> death;
+
+        // validate death rate
+        if (death < 1.0)
+        {
+            cout << "Death rate must be greater than 1.0!" << endl;
+            exit(0);        
+        }
+
+        // print the population size
+        float size = getPopSize(p, birth, death);
+        cout << "New population size: " << size << endl;
+    }
+}
+
+// returns the new population size
+float getPopSize(int p, float birthRate, float deathRate)
+{
+    return p + (birthRate * p) - (deathRate * p);
 }
