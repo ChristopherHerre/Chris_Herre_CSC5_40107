9d386459a58d07bb73efc3e59b674e0c79a43ef1
root <root@localhost.localdomain>
2017-02-17 14:19:04 -0800

    Initial commit

---
+++ Scripts/git_push.sh
@@ -0,0 +1,6 @@
+cd /home/Chris/Desktop/Chris_Herre_CSC5_40107
+git init
+git add .
+git commit -m "Initial commit"
+git remote add origin https://github.com/ChristopherHerre/Chris_Herre_CSC5_40107
+git push -f origin master
